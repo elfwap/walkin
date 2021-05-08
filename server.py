fdb7fed58fc6b9a0d83d946a7e40e49a1cc79b58
elfwap <muhammedtahirabdullahi@gmail.com>
2021-05-08 17:45:13 +0100

    server connector

---
+++ server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import socket
+
+# create a socket object 
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# get local machine name
+
+host = socket.gethostname()
+port = 9999
+ 
+# bind to the port 
+serversocket.bind((host, port))
+
+# queue up to 5 requests 
+serversocket.listen(5)
+
+while True:
+    # establish a connection
+    clientsocket,addr = serversocket.accept()
+ 
+    print("Got a connection from %s" % str(addr))
+    cmsg = clientsocket.recv(1024)
+    print('Message: {}'.format(cmsg))
+    msg='Thank you for connecting'+ "\r\n"
+    clientsocket.send(msg.encode('ascii'))
+    clientsocket.close()
